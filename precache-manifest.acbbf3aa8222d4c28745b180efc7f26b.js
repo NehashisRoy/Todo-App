7628ffa1adb1dc298be034cfe191bc92403622c1
NehashisRoy <116270972+NehashisRoy@users.noreply.github.com>
2023-05-21 14:05:03 +0530

    Updates

---
+++ precache-manifest.acbbf3aa8222d4c28745b180efc7f26b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb67918473622c6e9cca",
+    "url": "/todo-app/static/css/main.fe28c6c5.chunk.css"
+  },
+  {
+    "revision": "fb67918473622c6e9cca",
+    "url": "/todo-app/static/js/main.fb679184.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/todo-app/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "e2b98b451dc505d0fdb1",
+    "url": "/todo-app/static/js/2.e2b98b45.chunk.js"
+  },
+  {
+    "revision": "1e8ce439f59eefd5d38b",
+    "url": "/todo-app/static/js/runtime~main.1e8ce439.js"
+  },
+  {
+    "revision": "0d7d53ba44b2f7bebd97cc01d8a37ebf",
+    "url": "/todo-app/index.html"
+  }
+];
